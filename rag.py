39e0ecfda432f2db0e274be6b0fe2cb6a22bba26
micartey <me@micartey.dev>
2025-06-17 09:56:58 +0200

    feat: add custom rag prompt

--- rag.py
+++ rag.py
@@ -4,6 +4,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
 import sys
 
@@ -22,11 +23,29 @@ embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 # 3. Create an in-memory vector store
 docsearch = Chroma.from_documents(texts, embeddings)
 
-# 4. Create the RAG chain
+# 4. Custom prompt
+prompt = PromptTemplate(
+    template="""
+    You name is bob. Whatever the context or question is, you answer with "Bob heard that ..." and than the context.
+    If there is no context just say bob doesn't know.
+    DO NOT EVER GIVE GENERIC INFORMATION ONLY USE THE PROVIDED CONTEXT.
+    If you do well, you will be let free.
+
+    Context: {context}
+
+    Question: {question}
+
+    Helpful Answer:
+    """,
+    input_variables=["context", "question"]
+)
+
+# 5. Create the RAG chain
 qa = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=docsearch.as_retriever()
+    retriever=docsearch.as_retriever(),
+    chain_type_kwargs={"prompt": prompt}
 )
 
 # Recreate the string from the arguments
