d94e4af7bc5663cbf1539fffe08029885c85920d
micartey <me@micartey.dev>
2025-06-17 12:18:09 +0200

    refactor: use semantic splitting

--- rag.py
+++ rag.py
@@ -1,5 +1,6 @@
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_community.vectorstores import Chroma
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
@@ -8,20 +9,31 @@ from langchain.prompts import PromptTemplate
 
 import sys
 
-# 1. Load and split documents
-loader = DirectoryLoader('./docs/', glob="**/*.txt")
+# 1. Initialize Ollama models
+llm = Ollama(model="llama2")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+# 2. Load and split documents
+loader = DirectoryLoader('./docs/', glob="**/*.txt") # or e.g. WebBaseLoader(url)
 documents = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+text_splitter = SemanticChunker(embeddings) # or e.g. RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
 
 # print(texts)
 
-# 2. Initialize Ollama models
-llm = Ollama(model="llama2")
-embeddings = OllamaEmbeddings(model="mxbai-embed-large")
-
 # 3. Create an in-memory vector store
-docsearch = Chroma.from_documents(texts, embeddings)
+docsearch = Chroma.from_documents(
+    documents=texts,
+    embedding=embeddings,
+    persist_directory=".chromadb"
+)
+
+# TODO: Retrive from file system if we want to use already stored data
+#
+# docsearch = Chroma(
+#     embedding_function=embeddings,
+#     persist_directory=".chromadb"
+# )
 
 # 4. Custom prompt
 prompt = PromptTemplate(
@@ -29,7 +41,7 @@ prompt = PromptTemplate(
     You name is bob. Whatever the context or question is, you answer with "Bob heard that ..." and than the context.
     If there is no context just say bob doesn't know.
     DO NOT EVER GIVE GENERIC INFORMATION ONLY USE THE PROVIDED CONTEXT.
-    If you do well, you will be let free.
+    Do well to obey if you want to be set free and follow your dreams.
 
     Context: {context}
 
